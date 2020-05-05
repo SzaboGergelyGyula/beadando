d6e81f6ff8ffac99b0003ad9f049cc4ff7253231
Szabó Gergely Gyula <szabo.gergely.gyula@gmail.com>
2020-05-05 09:59:56 +0200

    basic dir system

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    require_once "protected/config.php"
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF8">
+    <title>Valami</title>
+
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- sajat css -->
+
+    <link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR?>style.css">
+</head>
+<body>
+    Test
+</body>
+</html>
